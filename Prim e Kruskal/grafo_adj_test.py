ec165d1357e1a156268677cac5fe80527acb166f
Kilvia Carvalho <carvalho.kilvia@gmail.com>
2019-05-27 20:59:19 -0300

    Roteiro 6

---
+++ Prim e Kruskal/grafo_adj_test.py
@@ -0,0 +1,18 @@
+from grafo_adj import *
+
+grafo1 = Grafo([], [])
+grafo2 = Grafo([], [])
+grafo3 = Grafo([], [])
+
+for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
+    grafo1.adiciona_vertice(i)
+for i in [('J-C', 3), ('C-E', 1), ('C-P', 5), ('C-M', 4), ('C-T', 2), ('M-T', 3), ('P-M', 1), ('T-Z', 6), ('M-Z', 9)]:
+    grafo1.adiciona_aresta(*i)
+
+for i in ['A', 'B', 'C', 'D', 'E']:
+    grafo2.adiciona_vertice(i)
+for i in [('A-B', 2), ('A-D', 2), ('B-C', 6), ('C-D', 4), ('C-E', 3), ('D-E', 5)]:
+    grafo2.adiciona_aresta(*i)
+
+print(grafo1.prim())
+print(grafo2.kruskal())
